904b63ec89ef0567425621200b69124323c8c3ca
Bronya Rand <40864681+Bronya-Rand@users.noreply.github.com>
2026-08-28 20:28:16 -0500

    chore: use NumericStringEntry

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabGame.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabGame.cs
@@ -46,9 +46,8 @@ public class SettingsTabGame : SettingsTab
             {
                 CheckVisibility = () => enableDiscordRpc.Value,
             },
-            new SettingsEntry<int>(Strings.DiscordRPCCustomPortSetting, Strings.DiscordRPCCustomPortDescription, () => Program.Config.DiscordRpcCustomPort ?? 2026, x => Program.Config.DiscordRpcCustomPort = x)
+            new NumericSettingsEntry(Strings.DiscordRPCCustomPortSetting, Strings.DiscordRPCCustomPortDescription, () => Program.Config.DiscordRpcCustomPort ?? 2026, x => Program.Config.DiscordRpcCustomPort = x, 1024, 49151, 0)
             {
-                CheckValidity = (port) => (port is < 1024 or > 49151) ? Strings.PortNumberMustBeBetween : null,
                 CheckVisibility = () => this.enableDiscordRpcCustomPort.Value,
             },
             new SettingsEntry<bool>(Strings.UseXLAuthMacrosSetting, Strings.UseXLAuthMacrosSettingDescription, () => Program.Config.IsOtpServer ?? false, x => Program.Config.IsOtpServer = x),
